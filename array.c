ddddae1a7de59da4b99cc0965b44170b5b9738c0
HarishmaKrishnakumar <46838369+HarishmaKrishnakumar@users.noreply.github.com>
2019-03-27 16:05:53 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+ int N,i,j,c=0;
+ scanf("%d",&N);
+ int a[N];
+ for(i=0;i<N;i++)
+ {
+     scanf("%d",&a[i]);
+ }
+ 
+ for(i=0;i<N;i++)
+ {
+     if(a[i]%2==0)
+     {
+         for(j=0;i<N;j++)
+          {
+              if(a[j]%2==0)
+                {
+                  
+                }
+         
+          }
+    
+    
+     }
+ }
+ 
+    return 0;
+}
